dc71dde467614984e4b65db94788ea157fe28227
Steb <steb.vs@outlook.com>
2023-08-04 16:07:42 +0200

    add physics layer + fix arrow physics + add arrow hit depth

--- Engine/Entities/Weapons/ArrowItem.cs
+++ Engine/Entities/Weapons/ArrowItem.cs
@@ -10,6 +10,13 @@ namespace PluieDeFleche.Engine.Entities.Weapons
 {
 	internal partial class ArrowItem : RigidBody3D
 	{
+		private const float LIFESPAN = 10.0f;
+		private double _airTimer;
+		private double _stuckTimer;
+		private Node _malemoniak;
+		private bool _hasHit;
+		private bool _isAttached;
+
 		public override void _Ready()
 		{
 			BodyEntered += ArrowItem_BodyEntered;
@@ -17,19 +24,50 @@ namespace PluieDeFleche.Engine.Entities.Weapons
 
 		private void ArrowItem_BodyEntered(Node body)
 		{
-			Freeze = true;
+			_hasHit = true;
 
 			if(body.GDTypeIs("chb_malemoniak"))
 			{
-				body.Call("death");
+				_malemoniak = body;
 			}
 		}
 
 		public override void _PhysicsProcess(double delta)
 		{
+			if(_hasHit)
+			{
+				Freeze = true;
+
+				if (!_isAttached && _malemoniak != null)
+				{
+					Transform3D oldTr;
+
+					_malemoniak.Call("take_damage", 2);
+					oldTr = GlobalTransform;
+					GetParent().RemoveChild(this);
+					_malemoniak.AddChild(this);
+					GlobalTransform = oldTr;
+					_isAttached = true;
+				}
+			}
+
 			if(Freeze)
 			{
+				_stuckTimer += delta;
+
+				if (_stuckTimer >= LIFESPAN)
+				{
+					QueueFree();
+				}
+
 				return;
+			}
+
+			_airTimer += delta;
+
+			if(_airTimer >= LIFESPAN)
+			{
+				QueueFree();
 			}
 
 			LookAt(GlobalTransform.Origin + LinearVelocity.Normalized());
